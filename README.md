0395a1459780ba9e2d5d6a37d81e7e7e1c8da681
Toocels <toocels@gmail.com>
2022-08-23 14:56:29 +0530

    Initial

---
+++ README.md
@@ -0,0 +1,1 @@
+Attempt to make flappy bird in terminal!
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using std::cout, std::endl, std::cin;
+
+int main(){
+
+	cout << "Hello World!" << endl;
+
+	return 0;
+}
